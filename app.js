0d7cd610e9f3cc31a8308b2814ab3e7c363ce5c4
Yassin-010 <YassinSE898@gmail.com>
2024-03-27 12:32:21 +0000

    dd

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
 var userName, gender, age;
+userName = '';
+gender = '';
 
-while (!userName) {
-    userName = prompt("Enter your name:");
-    if (!userName) {
-        alert("Please enter a valid name.");
-    }
+userName = prompt("Enter your name:");
+if (!userName) {
+    alert("You entered an invalid name.");
 }
 
-while (!gender || (gender !== "male" && gender !== "female")) {
-    gender = prompt("Enter your gender (male or female):");
-    if (!gender || (gender !== "male" && gender !== "female")) {
-        alert("Please enter a valid gender (male or female).");
-    }
+gender = prompt("Enter your gender (male or female):");
+if (!gender || (gender !== "male" && gender !== "female")) {
+    alert("You entered an invalid gender (male or female).");
+
 }
 
-while (!age || age <= 0) {
-    age = parseInt(prompt("Enter your age:"));
-    if (!age || age <= 0) {
-        alert("Please enter a valid age.");
-    }
+
+age = parseInt(prompt("Enter your age:"));
+if (!age || age <= 0) {
+    alert("You entered an invalid age.");
+
 }
 
 var skipMessage = confirm("Do you want to show the welcoming message?");
 
-if (skipMessage) {
-    var title;
-    if (gender === "male") {
-        title = "Mr";
-    } else if (gender === "female") {
-        title = "Ms";
-    }
 
-    var message = title ? "Welcome, " + title + " " + userName + "!" : "Welcome, " + userName + "!";
-    alert(message);
+var title = "";
+if (gender === "male") {
+    title = "Mr";
+} else if (gender === "female") {
+    title = "Ms";
 }
+var message = title ? "Welcome, " + title + " " + userName : "Welcome, " + userName;
+alert(message);
 
 // Get necessary elements
 const taskInput = document.querySelector('.task-input');
